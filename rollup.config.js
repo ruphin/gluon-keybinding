32858fe9c49c58e28741d9fb0954b062c071e142
Goffert van Gool <ruphin@ruphin.net>
2018-10-08 02:49:04 +0200

    Clean up examples and build, update to Gluon 2.5.1

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,53 @@
 import filesize from 'rollup-plugin-filesize';
-import uglify from 'rollup-plugin-uglify';
+import cleanup from 'rollup-plugin-cleanup';
+import { terser } from 'rollup-plugin-terser';
 import babel from 'rollup-plugin-babel';
 import includePaths from 'rollup-plugin-includepaths';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import * as path from 'path';
 
+const license = min =>
+  min
+    ? ''
+    : `/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2018 Goffert van Gool
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+`;
+
 const includePathOptions = {
-  paths: ['node_modules/@gluon/keybinding', '.'],
+  paths: ['node_modules/@gluon/gluon', '.'],
   extensions: ['.js']
 };
 
-function getConfig({ dest, format, uglified = true, transpiled = false, bundled = true }) {
+function getConfig({ dest, format, minified = false, transpiled = false, bundled = true }) {
   const conf = {
     input: 'src/gluon-keybinding.js',
-    output: { exports: 'named', file: dest, format, name: 'GluonKeybinding', sourcemap: uglified },
-    external: bundled ? [] : [path.resolve('./@gluon/gluon/gluon.js')],
+    output: { banner: license(minified), file: dest, format, name: 'GluonKeybinding', sourcemap: !minified },
+    external: [!bundled && path.resolve('./@gluon/gluon/gluon.js')].filter(Boolean),
     plugins: [
-      bundled && includePaths(includePathOptions),
+      includePaths(includePathOptions),
       transpiled && resolve(),
       transpiled &&
         commonjs({
@@ -28,15 +58,21 @@ function getConfig({ dest, format, uglified = true, transpiled = false, bundled 
           presets: [['env', { modules: false }]],
           plugins: ['external-helpers']
         }),
-      uglify({
-        warnings: true,
-        toplevel: uglified,
-        sourceMap: uglified,
-        compress: uglified && { passes: 2 },
-        mangle: uglified,
-        output: { beautify: !uglified }
-      }),
-      filesize()
+      // Remove duplicate license
+      !minified &&
+        cleanup({
+          maxEmptyLines: 1,
+          comments: [/^((?!\(c\) \d{4} Goffert)[\s\S])*$/]
+        }),
+      minified &&
+        terser({
+          warnings: true,
+          mangle: {
+            module: true
+          },
+          output: { preamble: license(minified) }
+        }),
+      minified && filesize()
     ].filter(Boolean)
   };
 
@@ -44,8 +80,8 @@ function getConfig({ dest, format, uglified = true, transpiled = false, bundled 
 }
 
 const config = [
-  getConfig({ dest: 'gluon-keybinding.es5.js', format: 'iife', transpiled: true }),
-  getConfig({ dest: 'gluon-keybinding.umd.js', format: 'umd' })
+  getConfig({ dest: 'gluon-keybinding.es5.js', format: 'iife', minified: true, transpiled: true }),
+  getConfig({ dest: 'gluon-keybinding.umd.js', format: 'umd', minified: true })
 ];
 
 export default config;
